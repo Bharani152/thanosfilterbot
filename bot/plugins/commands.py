12d01d9b300a686a0cd3905cc30795aff9f73cd6
Bharani152 <87841685+Bharani152@users.noreply.github.com>
2021-07-27 08:57:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = @tamillmovieshd,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '❤️ join Now ❤️', url="https://t.me/tamillmovieshd"
                                 )
                         ]
                     ]
